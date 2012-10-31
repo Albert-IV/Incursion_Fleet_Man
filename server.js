48613d1c36520ec8fc7f0372f6a11a9d231d8d00
Albert Engelbrecht <dropped.on.japan@gmail.com>
2012-10-31 00:59:27 -0500

    server.js: Fixed favicon location

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var connect = require('connect'),
 		_ = require('underscore'),
+		async = require('async'),
 		router = require('dispatch');
-
 
 var app = connect();
 
-
+function respond(req, res, next) {
+	res.end("<html><h1>Hello Capsuleers!</h1></html>")
+}
 
 var server = connect(
 	connect.logger('dev'),
@@ -14,7 +16,7 @@ var server = connect(
 	connect.bodyParser(),
 	connect.query(),
 	connect.static( __dirname + '/public' ),
-	connect.favicon('fav.ico'),
+	connect.favicon('./public/fav.ico'),
 	router({
 		"/": function(req, res, next) {
 			respond( req, res, next );
